a62895501ae9a4e251757ff04f918f9329f4a2f7
AnkitYande <ankityande@gmail.com>
2020-12-29 13:16:32 -0600

    Updates

---
+++ precache-manifest.432e232b9e9233341a941d791550e751.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d41b7e32fcbf26f93b743a18ea75cbf",
+    "url": "/TwitterCopy/index.html"
+  },
+  {
+    "revision": "c5cfbb6c79dfd7450b0b",
+    "url": "/TwitterCopy/static/css/main.c224693d.chunk.css"
+  },
+  {
+    "revision": "33151dd4af54de8429fe",
+    "url": "/TwitterCopy/static/js/2.37a32844.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TwitterCopy/static/js/2.37a32844.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5cfbb6c79dfd7450b0b",
+    "url": "/TwitterCopy/static/js/main.c0bd519d.chunk.js"
+  },
+  {
+    "revision": "c005c0621c6827a0bcc2",
+    "url": "/TwitterCopy/static/js/runtime-main.8b3f3937.js"
+  }
+]);
